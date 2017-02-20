17dfc513a1d4b92a08debe54f850810555f28245
Gary Pang <gary.pang@gmail.com>
2017-02-20 17:33:47 -0500

    Add Html Webpack plugin
    Plugin adds JavaScript to bottom of webpage before </body> tag.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './client/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
 module.exports = {
 	entry: './client/index.js',
 	output: {
@@ -10,5 +18,6 @@ module.exports = {
 			{ test: /\.js$/, loaders: 'babel-loader', exclude: /node_modules/},
 			{ test: /\.jsx$/, loaders: 'babel-loader', exclude: /node_modules/}
 		]
-	}
+	},
+	plugins: [HtmlWebpackPluginConfig]
 }
